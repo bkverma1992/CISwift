9ef51fe7ce3aa6fe97289ba72cc6aff1b3698269
Bhoopendra Kumar <bkvermaiet@gmail.com>
2026-09-10 14:43:10 +0530

    Add unit tests

---
+++ Dev_UIKitTests/Dev_UIKitTests.swift
@@ -0,0 +1,15 @@
+//
+//  Dev_UIKitTests.swift
+//  Dev_UIKitTests
+//
+//  Created by APPLE on 10/09/26.
+//
+
+import XCTest
+
+final class Dev_UIKitTests: XCTestCase {
+    func testExample() {
+        let result = 2 + 2
+        XCTAssertEqual(result, 4)
+    }
+}
