136167da13ee33087def2a02a75f3d56cf803614
Bhoopendra Kumar <bkvermaiet@gmail.com>
2026-09-11 17:35:44 +0530

    Clouser  function added

--- Dev_UIKit/ViewController.swift
+++ Dev_UIKit/ViewController.swift
@@ -16,8 +16,66 @@ class ViewController: UIViewController {
         let result = addData(2, 7)
         print(result)
         print(addData1(2, 7))
+        // MARK: - Optional Value
         makeOptional()
+//        MARK: - Clouser Value
+        makeClouser()
 
+    }
+    
+    func makeClouser() {
+//       MARK: - Clouser is a block of code that can be store in variable and user later. there are few type like non-escaping, escaping, traling, autoclouser,
+        
+        // MARK: - Traling Clouser
+        // in the function the last paramet if it is clouser tht is called traling clouser.
+        makeTrailingClouser(name: "Hello Bhupi") {
+            print("Hello team")
+        }
+        
+        // MARK: - AutoClouser
+        // @autoclosuer automaticaaly expression ko clouser meconvert krta hai jisme hme {} ye ni likhna pdta
+           makeAutoClouser(10>15)
+        
+        // MARK: - Non-esaping clouser
+        // Clouser jis function ko diya hai usi function me call ho jayega funtion end hone se phle. function k bahar bad me use nhi kiya ja skta
+        nonEscapingClouser {
+            print("Task DONE")
+        }
+        
+        // MARK: - Esaping Clouser
+        // Clouser use - it excute after the function end like async with @esacping
+        escapingClouser {
+            print("Escaping Clouser DONE")
+        }
+    }
+    // Traling clouser
+    func makeTrailingClouser(name: String, lastClsr: () -> Void) {
+        print(name)
+        lastClsr()
+        
+    }
+    
+    // AutoClouser
+    func makeAutoClouser(_ conditon: @autoclosure () -> Bool) {
+        print( conditon())
+    }
+    
+    // MARK: - Non escaping
+    func nonEscapingClouser(completion: () -> Void) {
+        print("Started")
+        print("Task Ongoing")
+        completion()
+        print("Task Completed")
+        print("Task Finished")
+    }
+    
+    // MARK: - Escaping
+    func escapingClouser(completion: @escaping () -> Void) {
+       print("Started")
+        DispatchQueue.main.async {
+            completion()
+        }
+        print("completed")
     }
 
     func makeFunction() {
