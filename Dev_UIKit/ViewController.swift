96a1be2cc60f0a8f8e74040d9f9a4c4108f6ed4a
Bhoopendra Kumar <bkvermaiet@gmail.com>
2026-09-11 15:44:49 +0530

    Optional value function added

--- Dev_UIKit/ViewController.swift
+++ Dev_UIKit/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         let result = addData(2, 7)
         print(result)
         print(addData1(2, 7))
+        makeOptional()
 
     }
 
@@ -30,6 +31,44 @@ class ViewController: UIViewController {
     func addData1(_ val1: Int, _ val2: Int) -> Bool {
        return val1 > val2
     }
+    
+    func makeOptional() {
+        // Optional: value ho bhi sakti hai aur nil bhi
+        var userName: String?
+        // Nil-coalescing
+        print(userName ?? "Bhupi")
+        
+        // Force unwrap
+       // print(userName!)
 
+        // Optional binding
+        if let name = userName {
+            print(name)
+        }
+
+      //  Optional Chaining
+        var user = User()
+         user = User(name: "Bhupi")
+        print(user.name?.count)
+        if let count = user.name?.count {
+            print(count)
+        }
+        /*
+         String?       → Optional
+         if let        → Safely unwrap
+         guard let     → Safely unwrap + exit if nil
+         ??            → Give default value
+         ?.            → Safely access property/method
+         !             → Force unwrap → ⚠️ can crash
+         */
+    }
+
+}
+
+class User {
+    var name: String?
+    init(name: String? = nil) {
+        self.name = name
+    }
 }
 
